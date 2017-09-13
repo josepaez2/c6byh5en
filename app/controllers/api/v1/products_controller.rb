df8b0b58fdc164d9ba295fcdde38d1c191a030e7
josepaez2 <josepaez_2@hotmail.com>
2017-09-13 10:03:28 -0500

    product edition and deletion with api

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -6,16 +6,16 @@ class Api::V1::ProductsController < ApplicationController
 skip_before_action :verify_authenticity_token
 # protect_from_forgery with: :null_session
   def index
-    # @products = Product.all
-    # render json: @products
+    @products = Product.all
+    render json: @products
     # revisando en la pantalla del servidor si está 
     # creando los productos y rails c irb confirma
     # HTTParty.post('http://localhost:3000/api/v1/products', body: { name: "Producto 3", price: 39}.to_json, headers: { 'Content-Type' => 'application/json' })
     # HTTParty.post('http://localhost:3000/api/v1/products', body: {price: 39}.to_json, headers: { 'Content-Type' => 'application/json' })
     #     @product = Product.find()
-    HTTParty.patch('http://localhost:3000/api/v1/products/5', body: {name: "Yeah!", price: 10000}.to_json, headers: { 'Content-Type' => 'application/json' })
+    # HTTParty.patch('http://localhost:3000/api/v1/products/5', body: {name: "Yeah!", price: 10000}.to_json, headers: { 'Content-Type' => 'application/json' })
         # @product = Product.find(params[:id])
-    # HTTParty.delete('http://localhost:3000/api/v1/products/6', body: {}.to_json, headers: { 'Content-Type' => 'application/json' })
+    # HTTParty.delete('http://localhost:3000/api/v1/products/4', body: {}.to_json, headers: { 'Content-Type' => 'application/json' })
   end
 
   def show
